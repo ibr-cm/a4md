1c6041c25a81ffc1ed0dded7b0f680adf5bf16f9
Bastian Kulke <kulke@ibr.cs.tu-bs.de>
2021-06-03 14:52:27 +0200

    Add MisbehaviorCaService

---
+++ src/artery/application/md/MisbehaviorTypes.h
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * @author  Joseph Kamel
+ * @email   josephekamel@gmail.com
+ * @date    28/11/2018
+ * @version 2.0
+ *
+ * SCA (Secure Cooperative Autonomous systems)
+ * Copyright (c) 2013, 2018 Institut de Recherche Technologique SystemX
+ * All rights reserved.
+ *******************************************************************************/
+
+#ifndef __VEINS_mbTypes_H_
+#define __VEINS_mbTypes_H_
+
+#include <iostream>
+
+namespace misbehaviorTypes {
+
+enum MisbehaviorTypes {
+    Benign = 0,
+    LocalAttacker,
+    GlobalAttacker,
+    SIZE_OF_ENUM
+};
+
+static const char* mbNames[] = { "Benign", "LocalAttacker","GlobalAttacker"};
+
+static const MisbehaviorTypes intMbs[] = { Benign, LocalAttacker,GlobalAttacker };
+
+static_assert(sizeof(misbehaviorTypes::mbNames)/sizeof(char*) == misbehaviorTypes::SIZE_OF_ENUM
+        , "sizes dont match");
+
+static_assert(sizeof(misbehaviorTypes::intMbs)/sizeof(MisbehaviorTypes) == misbehaviorTypes::SIZE_OF_ENUM
+        , "sizes dont match");
+
+
+}
+
+#endif
